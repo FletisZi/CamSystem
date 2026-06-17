331c5edfd5c439f84de93506309609c2d82f4fec
Rodrigo da Silva dos Santos <rodrigo.santos@thermasparkhotel.com.br>
2026-06-17 20:52:22 -0300

    melhoria salvamento de frame de imagem do video criado, corrigido bug

--- internal/camera/camera_save_capture.go
+++ internal/camera/camera_save_capture.go
@@ -37,35 +37,51 @@ func (c *Camera) SaveRecording() (string, error) {
 
 	fmt.Println("Salvando em:", filename)
 
-	cmd := exec.Command("ffmpeg",
-		"-f", "mpegts",
-		"-i", "pipe:0",
-		"-c", "copy",
-		"-movflags", "+faststart",
-		filename,
-	)
+	// cmd := exec.Command("ffmpeg",
+	// 	"-f", "mpegts",
+	// 	"-i", "pipe:0",
+	// 	"-c", "copy",
+	// 	"-movflags", "+faststart",
+	// 	filename,
+	// )
 
-	cmdFrame := exec.Command("ffmpeg",
-		"-f", "mpegts",
-		"-i", "pipe:0",
-		"-vf", "fps=1/2", // Extrai 1 frame por segundo
-		"-q:v", "2",    // Define a qualidade dos frames (1 é a melhor qualidade, 31 é a pior)
-		"-f", "image2pipe",
-		"-vcodec", "mjpeg",
-		// "-pix_fmt", "yuv420p",não sei o que é isso 
-		"pipe:1",
-		`c:\\Users\\rssantos\\Videos\\camsystem\\frames\\frame_%d.jpeg`, // Salva os frames como JPEGs numerados
-		// "C:\\Users\\rssantos\\Videos\\camsystem\\frames\\frame_111.jpg",
-	)
+	// cmdFrame := exec.Command(
+	// 	"ffmpeg",
+	// 	"-f", "mpegts",
+	// 	"-i", "pipe:0",
+	// 	"-vf", "fps=1",
+	// 	"-q:v", "2",
+	// 	"c:\\Users\\rssantos\\Videos\\camsystem\\frames\\frame_%03d.jpeg",
+	// )
 
-	stdinFrame, err := cmdFrame.StdinPipe()
-	if err != nil {
-		return "", err
-	}
+	cmd := exec.Command(
+    "ffmpeg",
 
-	if err := cmdFrame.Start(); err != nil {
-		return "", err
-	}
+    "-f", "mpegts",
+    "-i", "pipe:0",
+
+    // saída MP4
+    "-map", "0:v",
+    "-c:v", "copy",
+    "-movflags", "+faststart",
+    filename,
+
+    // saída JPEG
+    "-map", "0:v",
+    "-vf", "fps=1",
+    "-c:v", "mjpeg",
+    "-q:v", "2",
+    "c:\\Users\\rssantos\\Videos\\camsystem\\frames\\frame_%03d.jpeg",
+)
+
+	// stdinFrame, err := cmdFrame.StdinPipe()
+	// if err != nil {
+	// 	return "", err
+	// }
+
+	// if err := cmdFrame.Start(); err != nil {
+	// 	return "", err
+	// }
 
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
@@ -79,18 +95,37 @@ func (c *Camera) SaveRecording() (string, error) {
 	go func() {
 		defer stdin.Close()
 
-		for _, frame := range c.RecordingBuffer {
-			stdin.Write(frame)
+		for _, chunk  := range c.RecordingBuffer {
+			if _, err := stdin.Write(chunk); err != nil {
+				fmt.Printf("Erro ao escrever frame para FFmpeg: %v\n", err)
+				return
+			}
 		}
 	}()
 
-	go func() {
-		defer stdinFrame.Close()
+	// go func() {
+	// 	defer stdinFrame.Close()
 
-		for _, frame := range c.RecordingBuffer {
-			stdinFrame.Write(frame)
-		}
-	}()
+	// 	for _, chunk := range c.RecordingBuffer {
+	// 		if _, err := stdinFrame.Write(chunk); err != nil {
+	// 			fmt.Printf("Erro ao escrever frame para FFmpeg: %v\n", err)
+	// 			return
+	// 		}
+	// 	}
+	// }()
 
-	return filename, cmd.Wait()
+	videoErr := cmd.Wait()
+	// frameErr := cmdFrame.Wait()
+
+	if videoErr != nil {
+		fmt.Printf("Erro ao salvar vídeo: %v\n", videoErr)
+		return "", videoErr
+	}
+
+	// if frameErr != nil {
+	// 	fmt.Printf("Erro ao salvar frames: %v\n", frameErr)
+	// 	return "", frameErr
+	// }	
+
+	return filename, nil
 }
